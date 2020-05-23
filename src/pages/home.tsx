38becd74cdbc01cf90a37b6733880254284954b9
Simonwtaylor <Simonwtaylor93@gmail.com>
2020-05-23 15:05:35 +0100

    Reworked home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import { DragDropContext, DropResult } from 'react-beautiful-dnd';
-import { Grid, Input, Button } from 'semantic-ui-react';
+import { Grid } from 'semantic-ui-react';
 import { BoardColumn } from '../components/board';
-import { ITask } from '../lib';
+import { IBoard } from '../lib';
 
 export interface IHomePageProps {
   socket?: SocketIOClient.Socket;
@@ -11,26 +11,19 @@ export interface IHomePageProps {
 const HomePage: React.FC<IHomePageProps> = ({
   socket,
 }) => {
-  const [load, setLoad] = React.useState(false);
-  const [items, setItems] = React.useState<ITask[]>([]);
-  const [selected, setSelected] = React.useState<ITask[]>([]);
-  const [boardOneText, setBoardOneText] = React.useState<string>("");
-  const [boardTwoText, setBoardTwoText] = React.useState<string>("");
-
+  //const [load, setLoad] = React.useState(false);
+  const [boards, setBoards] = React.useState<IBoard[]>([]);
 
   React.useEffect(() => {
     if (socket) {
-      socket.emit('getTasks');
+      socket.emit('getBoards');
     }
-  }, [load, socket])
+  }, [socket])
 
   React.useEffect(() => {
-    if (socket) {
-      socket.on('tasks', (tasks: ITask[]) => {
-        setItems(tasks.filter(a => a.boardId === 'droppable'));
-        setSelected(tasks.filter(a => a.boardId === 'droppable2'));
-      });
-    } 
+    socket?.on('boards', (boards: IBoard[]) => {
+      setBoards(boards);
+    });
   }, [socket]);
 
   const onDragEnd = (result: DropResult) => {
@@ -45,42 +38,35 @@ const HomePage: React.FC<IHomePageProps> = ({
       // TODO: Add reordering
       return;
     } else {
-      if (destination.droppableId === "droppable") {
-        const newItem = selected.find(a => a.id === draggableId);
-        const newItems = [...items];
+      
+      const boardsToEdit = [...boards];
+      const board = boardsToEdit
+      .find(
+        a => a.id === +source.droppableId
+      );
 
-        if (newItem) {
-          newItems.push(newItem);
-        }
+      const taskToAdd = board?.tasks.find(a => a.id === draggableId);
+      
+      const removeFrom = board?.tasks
+      .findIndex(
+        a => a.id === draggableId,
+      );
 
-        setItems(newItems);
-        setSelected(selected.filter(a => a.id === draggableId))
-      } else {
-        const newItem = items.find(a => a.id === draggableId);
-        const newItems = [...selected];
+      if (removeFrom) {
+        boardsToEdit.find(
+          a => a.id === +source.droppableId
+        )?.tasks
+        .splice(
+          removeFrom, 1
+        );
+      }
 
-        if (newItem) {
-          newItems.push(newItem);
-        }
-
-        setSelected(newItems);
-        setItems(items.filter(a => a.id === draggableId))
+      if (taskToAdd) {
+        boardsToEdit.find(a => a.id === +destination.droppableId)?.tasks.push(taskToAdd);
       }
 
       socket?.emit('updateTaskBoard', {id: draggableId, boardId: destination.droppableId });
     }
-  };
-
-  const addTaskToBoardOne = () => {
-    setLoad(!load);
-    socket?.emit('addTask', { content: boardOneText, boardId: 'droppable'});
-    setBoardOneText('');
-  };
-
-  const addTaskToBoardTwo = () => {
-    setLoad(!load);
-    socket?.emit('addTask', { content: boardTwoText, boardId: 'droppable2'});
-    setBoardTwoText('');
   };
 
   return (
@@ -88,33 +74,17 @@ const HomePage: React.FC<IHomePageProps> = ({
       <Grid
         style={{ padding: '15px' }}
       >
-        <Grid.Row columns={2}>
-          <Grid.Column>
-            <h3 style={{color: 'white'}}>List 1</h3>
-            <div
-              style={{ paddingTop: '5px', paddingBottom: '5px' }}
-            >
-              <Input 
-                value={boardOneText}
-                onChange={(e: React.ChangeEvent<HTMLInputElement>) => setBoardOneText(e.target.value)}
-              />
-              <Button color={'green'} type={'button'} onClick={addTaskToBoardOne}>add</Button>
-            </div>
-            <BoardColumn items={items} droppableId={"droppable"} />
-          </Grid.Column>
-          <Grid.Column>
-            <h3 style={{color: 'white'}}>List 2</h3>
-            <div
-              style={{ paddingTop: '5px', paddingBottom: '5px' }}
-            >
-              <Input 
-                value={boardTwoText}
-                onChange={(e: React.ChangeEvent<HTMLInputElement>) => setBoardTwoText(e.target.value)}
-              />
-              <Button color={'green'} type={'button'} onClick={addTaskToBoardTwo}>add</Button>
-            </div>
-            <BoardColumn items={selected} droppableId={"droppable2"} />
-          </Grid.Column>
+        <Grid.Row columns={4}>
+          {
+            boards.map(({ tasks: boardTasks, name, id }: any) => {
+              return (
+                <Grid.Column>
+                  <h3 style={{color: 'white'}}>{name}</h3>
+                  <BoardColumn items={boardTasks} droppableId={`${id}`} key={`boardid${id}`} />
+                </Grid.Column>
+              )
+            })
+          }
         </Grid.Row>
       </Grid>
     </DragDropContext>
