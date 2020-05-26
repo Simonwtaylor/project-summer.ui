af2b8b9cb1b670c6abd63aee0910fd082962807c
Simonwtaylor <Simonwtaylor93@gmail.com>
2020-05-26 09:46:46 +0100

    Added sprint redux

--- src/redux/root-reducer.ts
+++ src/redux/root-reducer.ts
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import { UserReducer } from './user/user.reducer';
+import SprintReducer from './sprint/sprint.reducer';
 
 export default combineReducers({
   user: UserReducer,
+  sprint: SprintReducer,
 });
